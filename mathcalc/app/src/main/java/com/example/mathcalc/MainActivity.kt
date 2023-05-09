693e3bca7e758cb897976915fad7f80bb5f5df5f
youand18 <78105077+youand18@users.noreply.github.com>
2023-05-09 11:54:00 -0600

    initial app commit

---
+++ mathcalc/app/src/main/java/com/example/mathcalc/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mathcalc
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val numberInput = findViewById<EditText>(R.id.numberInput)
+        val calculateButton = findViewById<Button>(R.id.calculateButton)
+        
+    }
+}
