ce0777cd0232d8eb18009e875486787a326b0da0
youand18 <78105077+youand18@users.noreply.github.com>
2023-05-09 14:01:53 -0600

    calculator displays

--- mathcalc/app/src/main/java/com/example/mathcalc/MainActivity.kt
+++ mathcalc/app/src/main/java/com/example/mathcalc/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.mathcalc
 
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlin.math.pow
+import kotlin.math.sqrt
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -11,6 +14,25 @@ class MainActivity : AppCompatActivity() {
 
         val numberInput = findViewById<EditText>(R.id.numberInput)
         val calculateButton = findViewById<Button>(R.id.calculateButton)
-        
+
+        val minResult = findViewById<TextView>(R.id.minResult)
+        val avgResult = findViewById<TextView>(R.id.avgResult)
+        val maxResult = findViewById<TextView>(R.id.maxResult)
+        val stdDevResult = findViewById<TextView>(R.id.stdDevResult)
+
+        calculateButton.setOnClickListener {
+            val numbers = numberInput.text.toString().split(",").map { it.trim().toDouble() }
+            val min = numbers.minOrNull()
+            val avg = numbers.average()
+            val max = numbers.maxOrNull()
+            val stdDev = sqrt(numbers.map {it-avg}.map {it*it}.average())
+
+            minResult.text = "Min: $min"
+            avgResult.text = "Average: $avg"
+            maxResult.text = "Max: $max"
+            stdDevResult.text = "Standard Deviation: $stdDev"
+        }
+
+
     }
 }
